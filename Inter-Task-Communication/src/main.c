45dd67c171f5dc54ff5e1da5f87902d988c5d47e
Aburek Emmanuel <aburekemmanuel@gmail.com>
2026-09-08 12:54:45 +0300

    feat: implement FreeRTOS counting semaphore example for inter-task resource management on ESP32

--- Inter-Task-Communication/src/main.c
+++ Inter-Task-Communication/src/main.c
@@ -4,52 +4,67 @@
 #include "freertos/semphr.h"
 #include "driver/gpio.h"
 
-// Define the GPIO pin connected to the warning LED (GPIO 2 according to diagram.json)
-#define WARNING_LED GPIO_NUM_2
+// Define the connection pins
+#define LED_GREEN GPIO_NUM_4
+#define LED_BLUE GPIO_NUM_5
 
-// Define the semaphore handler globally
-static SemaphoreHandle_t alertSem = NULL;
+// Global handle for the counting semaphore
+static SemaphoreHandle_t  ledSem = NULL;
 
-// Signaler: Decides when an alert happens
-void sensor_alert_task(void *pvParameters) {
-    while (1) {
-        //Simulate waiting for a condition every 4 seconds
-        vTaskDelay(pdMS_TO_TICKS(4000));
+// Worker function that will be used to create the competing tasks
+void worker_task(void *pvParameters) {
 
-        printf("[Sensor] Threshold breached! Triggering warning LED...\n");
+    int task_id = (int)pvParameters;  // Takes the task ID from the argument passed during task creation
 
-        //Give the semaphore - count (0 --> 1)
-        xSemaphoreGive(alertSem);
-    }
-}
+    while(1){
+        // Tasks request LEDs at staggered intervals
+        vTaskDelay(pdMS_TO_TICKS(1500 * task_id));
 
+        printf("[Task %d] Needs an LED to show activity. Checking availability...\n", task_id);
 
-// Worker: Waits for the signal to pulse the LED
-void led_blink_task(void *pvParameters) {
-    while(1) {
-        // Blocks indefinitely until the sensor task calls xSemaphoreGive()
-        if(xSemaphoreTake(alertSem, portMAX_DELAY) == pdTRUE) {
-            //Once the semaphore is received, the LED turns on for 500ms
-            gpio_set_level(WARNING_LED,1); //Turn LED ON
+        // Try to take 1 token from the pool.
+        // If count > 0, decrements count and proceeds immediately.
+        // If count == 0, task enters Blocked state until someone calls xSemaphoreGive().
 
-            printf("[LED] Warning signal received. Blinking LED for 500ms...\n");
-            vTaskDelay(pdMS_TO_TICKS(500)); // Keep ON for 500ms
-            
-            gpio_set_level(WARNING_LED, 0); // Turn LED OFF
+        if(xSemaphoreTake(ledSem, portMAX_DELAY) == pdTRUE) {
+            printf("[Task %d] >> ACQUIRED an LED! (Led count decremented)\n", task_id);
+
+            gpio_num_t acquired_pin = (task_id %2 == 0) ? LED_BLUE : LED_GREEN;
+            gpio_set_level(acquired_pin, 1);
+
+            // Hold the resource for 3 seconds of "work"
+            vTaskDelay(pdMS_TO_TICKS(3000));
+
+            // Done with the LED
+            gpio_set_level(acquired_pin, 0);
+            printf("[Task %d] << FINISHED work. Returning LED to pool.\n", task_id);
+
+            // Return 1 token to the pool (increments count)
+            xSemaphoreGive(ledSem);
         }
-        
     }
-}
+} 
 
 void app_main(void) {
+    // Init GPIOs
+    gpio_reset_pin(LED_GREEN);
+    gpio_reset_pin(LED_BLUE);
+    gpio_set_direction(LED_GREEN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(LED_BLUE, GPIO_MODE_OUTPUT);
 
-    // 1. Set the mode of the pin
-    gpio_set_direction(WARNING_LED, GPIO_MODE_OUTPUT);
-    // 1. Initialize the semaphore
-    alertSem = xSemaphoreCreateBinary();
+    // Create counting semaphore:
+    // Parameter 1: max_count (Maximum capacity = 2)
+    // Parameter 2: initial_count (Available right now = 2)
+    ledSem = xSemaphoreCreateCounting(2,2);
 
-    xTaskCreate(sensor_alert_task, "sensor", 2048, NULL, 1, NULL);
-    xTaskCreate(led_blink_task,    "led",    2048, NULL, 2, NULL);
+    if(ledSem == NULL) {
+        printf("Failed to create counting semaphore");
+        return;
 
+    }
+
+    //We create the 3 competing tasks
+    xTaskCreate(worker_task, "worker1", 2048, (void *)1, 1, NULL);
+    xTaskCreate(worker_task, "worker2", 2048, (void *)2, 1, NULL);
+    xTaskCreate(worker_task, "worker3", 2048, (void *)3, 1, NULL);
 }
-
